08e25672e838cf703feb3999ef01abecff1c56c6
KrzysztofGier <104378746+KrzysztofGier@users.noreply.github.com>
2022-04-27 15:51:30 +0200

    Add print

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 neighbors = {'Germany': 'Berlin','Czech Republic':'Prague','Slovakia':'Bratislava','Ukraine':'Kiyv','Belarus':'Minsk','Lithuania':'Vilnius'}
 for item in neighbors.items():
     print(item)
+    print("Pozdrowienia od Krzyska")
